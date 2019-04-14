5735d2282d815e4c32ace67dbfb608894a1f19d3
Jorge Alberto Lopez <jlobeto@gmail.com>
2019-04-14 13:08:55 -0500

    Collapsing Toolbar Layout Ejemplo de como agregar un collapsing toolbar

--- app/src/main/java/com/example/jlopez/platzigram/adapter/PictureAdapterRecyclerView.java
+++ app/src/main/java/com/example/jlopez/platzigram/adapter/PictureAdapterRecyclerView.java
@@ -1,6 +1,7 @@
 package com.example.jlopez.platzigram.adapter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import com.example.jlopez.platzigram.R;
 import com.example.jlopez.platzigram.model.Picture;
+import com.example.jlopez.platzigram.view.PictureDetailActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -42,6 +44,15 @@ public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdap
         pictureViewHolder.timeCard.setText(picture.getTime());
         pictureViewHolder.likeNumberCard.setText(picture.getLike_number());
         Picasso.get().load(picture.getPicture()).into(pictureViewHolder.pictureCard);
+
+        pictureViewHolder.pictureCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, PictureDetailActivity.class);
+                activity.startActivity(intent);
+
+            }
+        });
 
 
     }
---
+++ app/src/main/java/com/example/jlopez/platzigram/view/PictureDetailActivity.java
@@ -0,0 +1,29 @@
+package com.example.jlopez.platzigram.view;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.jlopez.platzigram.R;
+
+public class PictureDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_detail);
+        showToolbar("",true);
+    }
+
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsingToolbar);
+
+
+    }
+}
