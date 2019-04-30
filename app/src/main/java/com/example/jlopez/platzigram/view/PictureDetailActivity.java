a3680198909f5919303d16d97c1a09ae317396a8
Jorge Alberto Lopez <jlobeto@gmail.com>
2019-04-29 23:22:26 -0500

    Transiciones en android (Activities)

--- app/src/main/java/com/example/jlopez/platzigram/view/PictureDetailActivity.java
+++ app/src/main/java/com/example/jlopez/platzigram/view/PictureDetailActivity.java
@@ -1,9 +1,12 @@
 package com.example.jlopez.platzigram.view;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.transition.Fade;
 import android.view.View;
 
 import com.example.jlopez.platzigram.R;
@@ -15,6 +18,14 @@ public class PictureDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_detail);
         showToolbar("",true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setEnterTransition(new Fade());
+            getWindow().setStatusBarColor(Color.TRANSPARENT);//Para cambiar el statusColor solo de esta activity
+        }
+
+
+
     }
 
     public void showToolbar(String tittle, boolean upButton){
